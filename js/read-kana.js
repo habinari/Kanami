b2f5eae3abbbd4efb8c365c606ff280a88133197
Javier Parada Tallante <javierparada73@outlook.com>
2018-05-04 17:35:37 +0200

    Gestion interfaz settings
    Gestiona los eventos básicos de la modal de settings (sin hacer nada en la estructura de datos). Checks de checkbox.

--- js/read-kana.js
+++ js/read-kana.js
@@ -1,12 +1,41 @@
 var kanas = [];
+var myKanas = [];
+
 var currentKana;
+var currentSyllabary = 'HIRAGANA';
 
 var $kana;
-var $checkInput;
 var $showButton;
 
 $.getJSON('data/kana.json', function(data){
     kanas = data.kanas;
+});
+
+$('#settingsModal').find('input[type="checkbox"]').click(function(){
+    isChecked = $(this).prop('checked');
+    if($(this).attr('id').indexOf("all") >= 0){
+        $(this).closest('div[class="row"]').find('input[type="checkbox"]').prop('checked', isChecked);
+    } else {
+        var mustCheck;
+        if(!isChecked){
+            mustCheck = false;
+        } else {
+            mustCheck = true;
+            $(this).closest('div[class="row"]').find('input[type="checkbox"]').each(function(){
+                if($(this).attr('id').indexOf("all") && !$(this).prop('checked'))
+                    mustCheck = false;
+            });
+        }
+        $(this).closest('div[class="row"]').find('input[type="checkbox"]').eq(0).prop('checked', mustCheck);
+    }
+});
+
+$('#settingsModal').find('button[name="apply"]').click(function(){
+    myKana = [];
+    var $radios = $('#settingsModal').find('input[type="radio"]');
+    currentSyllabary = $radios.eq(0).prop('checked') ? 'HIRAGANA' : 'KATAKANA';
+
+    $('#settingsModal').modal('hide');
 });
 
 $(document).ready(function(){
@@ -18,7 +47,6 @@ $(document).ready(function(){
         var rnd = Math.floor(Math.random() * kanas.length);
         currentKana = kanas[rnd];
         $kana.text(currentKana.character);
-        var msg =
         $($showButton).attr('data-original-title', currentKana.romaji);
         $($showButton).attr('data-content', 'Kana: ' + currentKana.character +
             ', Romaji: ' + currentKana.romaji +
