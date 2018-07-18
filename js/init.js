300e8adb34f15bfa87a40dcd1ada02a1c0c2a8a9
Javier Parada Tallante <javierparada73@outlook.com>
2018-07-18 18:48:04 +0200

    Simplifica la información de los settings

--- js/init.js
+++ js/init.js
@@ -15,4 +15,25 @@ $.getJSON('data/kana.json', function(data){
     $('[data-toggle="popover"]').popover();
 
     $('#settingsModal button[name="apply"]').click();
+
+    var $row = $('template#row');
+    var $kanaCheckers;
+    var lastRow = -1;
+    for(var i = 0; i < kanas.length; i++)
+    {
+        if(lastRow != kanas[i].row)
+        {
+            lastRow = kanas[i].row;
+            if(kanas[i].syllabary == "H")
+            {
+                $kanaCheckers = $('div[name="hiraganas"]');
+            } else {
+                $kanaCheckers = $('div[name="katakanas"]');
+            }
+            $kanaCheckers.append($row.html());
+            $kanaCheckers.find('.row').last().find('label').text(kanas[i].romaji.substring(0,1));
+
+        }
+    }
+
 });
