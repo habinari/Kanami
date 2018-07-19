9f012e28197072990727f454c97a2877df7b05d2
Javier Parada Tallante <javierparada73@outlook.com>
2018-07-19 20:11:54 +0200

    lectura y mostrado de datos realizado correctamente

--- js/init.js
+++ js/init.js
@@ -17,6 +17,7 @@ $.getJSON('data/kana.json', function(data){
     $('#settingsModal button[name="apply"]').click();
 
     var $row = $('template#row');
+    var $column = $('template#column');
     var $kanaCheckers;
     var lastRow = -1;
     for(var i = 0; i < kanas.length; i++)
@@ -32,8 +33,37 @@ $.getJSON('data/kana.json', function(data){
             }
             $kanaCheckers.append($row.html());
             $kanaCheckers.find('.row').last().find('label').text(kanas[i].romaji.substring(0,1));
-
+            $kanaCheckers.find('.row').last().find('input').attr('id', "all" +
+                kanas[i].syllabary + "-" + kanas[i].romaji.substring(0,1));
+                $kanaCheckers.find('.row').last().find('input').val(kanas[i].syllabary + "-" +
+                    kanas[i].romaji.substring(0,1));
         }
+
+        var $currentRow = $kanaCheckers.find('.row').last().find('.col-8');
+        $currentRow.append($column.html());
+        $currentRow.find('div').last().find('label').text(kanas[i].character);
+        var kanaId = kanas[i].syllabary + "-" + kanas[i].romaji;
+        $currentRow.find('div').last().find('input').attr('id', "check" + kanaId);
+        $currentRow.find('div').last().find('input').val(kanaId);
     }
 
+    $('#settingsModal input[type="radio"]').first().is(':checked', true);
+    $('#settingsModal input[type="radio"]').last().is(':checked', false);
+    $('#katakanas').hide();
+
+    $('#settingsModal input[type="checkbox"]').click(function(){
+        var isChecked = $(this).is(':checked');
+        if($(this).attr('id').includes("all")){
+            $(this).closest('div[class="row"]').find('input[type="checkbox"]').prop('checked', isChecked);
+        } else {
+            var mustCheck = isChecked;
+            if(isChecked) {
+                $(this).closest('div.row').find('input[type="checkbox"]').each(function(){
+                    if(!$(this).attr('id').includes('all') && !$(this).is(':checked'))
+                        mustCheck = false;
+                });
+            }
+            $(this).closest('div.row').find('input[type="checkbox"]').first().prop('checked', mustCheck);
+        }
+    });
 });
--- js/settings.js
+++ js/settings.js
@@ -1,23 +1,7 @@
-$('#settingsModal input[type="checkbox"]').click(function(){
-    var isChecked = $(this).is(':checked');
-    if($(this).attr('id').indexOf("all") >= 0){
-        $(this).closest('div[class="row"]').find('input[type="checkbox"]').prop('checked', isChecked);
-    } else {
-        var mustCheck = isChecked;
-        if(isChecked) {
-            $(this).closest('div[class="row"]').find('input[type="checkbox"]').each(function(){
-                if($(this).attr('id').includes('all') && !$(this).is(':checked'))
-                    mustCheck = false;
-            });
-        }
-        $(this).closest('div[class="row"]').find('input[type="checkbox"]').first().prop('checked', mustCheck);
-    }
-});
-
 $('#settingsModal input[type="radio"]').click(function(){
     var isHiragana = $('#settingsModal input[type="radio"]').first().is(':checked');
-    $('div[name="hiraganas"]').toggle( isHiragana );
-    $('div[name="hiraganas"]').toggle( ! isHiragana );
+    $('#hiraganas').toggle( isHiragana );
+    $('#katakanas').toggle( ! isHiragana );
 });
 
 $('#settingsModal button[name="apply"]').click(function(){
