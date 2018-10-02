67066f3b3c58039126820d91f7aff487eb8839a5
Javier Parada Tallante <javierparada73@outlook.com>
2018-10-02 19:58:12 +0200

    Hello world in electron

---
+++ index.js
@@ -0,0 +1,32 @@
+const electron = require('electron');
+const {app, BrowserWindow} = electron;
+
+const url = require('url');
+const path = require('path');
+  
+let mainWindow;
+
+function createWindow() {
+
+  mainWindow = new BrowserWindow({width: 800, height: 600})
+
+  mainWindow.loadFile(path.join(__dirname, 'index.html'));
+
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  })
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
