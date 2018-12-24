4aec2d256c8d3e4776fd68f759fdad8cba7ee620
Javier Parada Tallante <javierparada73@outlook.com>
2018-12-24 18:22:59 +0100

    fix static files

---
+++ src/renderer/App.vue
@@ -0,0 +1,170 @@
+<template>
+    <div>
+        <h1>{{selectedSyllabary}}</h1>
+
+    </div>
+</template>
+
+<script>
+
+import syll from '../../static/syllabaries.json';
+
+export default {
+    data: function() {
+        return {
+            
+            currentKana: {
+              "character": "#",
+              "romaji": "#",
+            },
+            syllabaries: {
+              hiragana: [],
+              katakana: []
+            },
+
+            selectedSyllabary: 'HIRAGANA',
+            selectedKanas: [],
+            unusedKanas: [],
+
+            isCorrectAnswer: false,
+            isAnswered: false,
+
+            showSettings: false,
+            showKanaInfo: false,
+
+            score: {
+              next: -1,
+              show: 0,
+              correctChecks: 0,
+              wrongChecks: 0,
+            }   
+        }
+    },
+
+    components: {
+        
+    },
+
+    created: function() {
+      this.getSyllabaries();
+    },
+
+    methods: {
+      nextKana: function(){
+
+        if(this.$data.selectedKanas.length == 0){
+          this.updateCheckedKanas();
+        }
+
+        if(this.$data.unusedKanas.length == 0){
+          this.$data.unusedKanas = this.$data.selectedKanas.slice();
+        }
+
+        this.$data.isAnswered = false;
+        this.$data.showKanaInfo = false;
+
+        var randomKana = Math.floor((Math.random() * this.$data.unusedKanas.length));
+
+        this.$data.currentKana = this.$data.unusedKanas[randomKana];
+        this.$data.unusedKanas.splice(randomKana, 1);
+        
+        document.querySelector('#checker').value = '';
+
+        this.$data.score.next++;
+      },
+
+      showAnswer: function(){
+        this.$data.showKanaInfo = true;
+
+        this.$data.score.show++;
+      },
+
+      checkAnswer: function(){
+        this.$data.isCorrectAnswer = 
+            document.querySelector('#checker').value.toUpperCase() == this.$data.currentKana.romaji;
+        
+        if(!this.$data.isAnswered)
+          (this.$data.isCorrectAnswer) ? this.$data.score.correctChecks++ : this.$data.score.wrongChecks++;
+
+        this.$data.isAnswered = true;
+      },
+
+      getSyllabaries: function () {
+        this.$data.syllabaries = syll;
+      },
+
+      
+      toggleSettings: function(){
+        this.$data.showSettings = !this.$data.showSettings;
+      },
+      toggleSyllabary: function(syllabary){
+        this.$data.selectedSyllabary = syllabary;
+      },
+
+      updateCheckedKanas: function(){
+        var $this = this;
+
+        this.$data.selectedKanas = [];
+        var syllabary = (this.$data.selectedSyllabary == 'HIRAGANA') ? this.$data.syllabaries.hiragana : this.$data.syllabaries.katakana;
+
+        for(var row = 0; row < syllabary.length; row++){
+          for(var item = 0; item < syllabary[row].length; item++){
+            if(syllabary[row][item].checked)
+              $this.$data.selectedKanas.push(
+                syllabary[row][item]
+              );
+          }
+        }
+
+        this.$data.unusedKanas = this.$data.selectedKanas.slice();
+
+        this.$data.score.next = -1;
+        this.$data.score.show = 0;
+        this.$data.score.correctChecks = 0;
+        this.$data.score.wrongChecks = 0;
+        
+      },
+
+      updateKanaRowCheckBox: function(row, mustCheck){
+        var syllabary = (this.$data.selectedSyllabary == 'HIRAGANA') ? this.$data.syllabaries.hiragana : this.$data.syllabaries.katakana;
+
+        for(var i = 0; i < syllabary[row].length; i++){
+          syllabary[row][i].checked = mustCheck;
+        }
+      }
+    },
+
+    computed: {
+      answer: function(){
+        if (this.$data.isAnswered && this.$data.isCorrectAnswer){
+          return 'is-success';
+        } else if(this.$data.isAnswered){
+          return 'is-danger';
+        } else {
+          return '';
+        }
+      },
+
+      currentSyllabary: function(){
+        if(this.$data.selectedSyllabary == 'HIRAGANA'){
+          return this.$data.syllabaries.hiragana;
+        } else {
+          return this.$data.syllabaries.katakana;
+        }
+      }
+    },
+
+    filters: {
+      capitalize: function (str) {
+        return str.toLowerCase()
+          .replace( /\b./g, val => val.toUpperCase() );
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+    @import "~bulma/css/bulma.css";
+    @import "~@fortawesome/fontawesome-free/css/all.css";
+    @import "./styles/margin.scss";
+</style>
